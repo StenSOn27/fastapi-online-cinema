a08125421cb14dfabef0ad9d96c6fe1fa4dcedc3
StenSOn27 <tarashevchik27@gmail.com>
2025-09-04 23:51:42 +0300

    connect sqlite db

---
+++ src/database/session_sqlite.py
@@ -0,0 +1,6 @@
+﻿from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+DATABASE_URL = "sqlite+aiosqlite:///./database.db"
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+AsyncSession = async_sessionmaker(engine, expire_on_commit=False)
