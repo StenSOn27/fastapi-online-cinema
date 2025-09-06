62a4ee7794eb4f4a54efc591cfe63743e80987ab
Taras Shevchyk <tarashevchik27@gmail.com>
2025-09-06 12:23:14 +0300

    implement get_db dependency

--- src/database/session_sqlite.py
+++ src/database/session_sqlite.py
@@ -1,6 +1,14 @@
-﻿from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+﻿from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import (
+    create_async_engine, async_sessionmaker, AsyncSession
+)
 
 DATABASE_URL = "sqlite+aiosqlite:///./database.db"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSession = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSession as db:
+        yield db
