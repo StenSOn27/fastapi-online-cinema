8a529d0686e1488485f795c789d05300ea3b412a
StenSOn27 <tarashevchik27@gmail.com>
2025-09-11 22:48:14 +0000

    change create user endpoint logic and add email sending support

--- src/routes/accounts.py
+++ src/routes/accounts.py
@@ -1,45 +1,84 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Request, status, BackgroundTasks
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.schemas.accounts import UserRegisterRequestSchema
 from src.database.models.accounts import ActivationTokenModel, UserGroupEnum, UserModel, UserGroupModel
 from src.database.session_sqlite import get_db
 from sqlalchemy import select
 from src.utils import hash_password
+from src.config.dependencies import get_accounts_email_notificator
+from src.notifications.emails import EmailSenderInterface
 
 
 router = APIRouter()
 
-@router.post("register/")
+@router.post("/register/")
 async def register_user(
+    request: Request,
     user_data: UserRegisterRequestSchema,
-    db: AsyncSession = Depends(get_db)
+    background_tasks: BackgroundTasks,
+    db: AsyncSession = Depends(get_db),
+    email_sender: EmailSenderInterface = Depends(get_accounts_email_notificator),
 ):
-    result = await db.execute(select(UserModel).where(email=user_data.email))
+
+    result = await db.execute(select(UserModel).filter_by(email=user_data.email))
     existing_user = result.scalars().first()
+
     if existing_user:
         raise HTTPException(status_code=400, detail="User already exists")
 
-    default_group = await db.execute(
-        select(UserGroupModel)
-        .filter_by(name=UserGroupEnum.USER)
-    ).scalars().first()
+    stmt = select(UserGroupModel).filter(UserGroupModel.id == 1)
+    result = await db.execute(stmt)
+    user_group = result.scalars().first()
+    print(user_group)
+    if not user_group:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Default user group not found."
+        )
 
     hashed_password = hash_password(user_data.password)
 
-    new_user = UserModel(
-        email=user_data.email,
-        _hashed_password=hashed_password,
-        is_active=False,
-        group=default_group
-    )
+    try:
+        new_user = UserModel(
+            email=user_data.email,
+            _hashed_password=hashed_password,
+            is_active=False,
+            group=user_group
+        )
+        db.add(new_user)
+        await db.flush()
 
-    activation_token = ActivationTokenModel(user=new_user)
+        existing_token_result = await db.execute(
+            select(ActivationTokenModel).filter_by(user_id=new_user.id)
+        )
+        existing_token = existing_token_result.scalars().first()
+        if existing_token:
+            await db.delete(existing_token)
+            await db.flush()
+        
+        activation_token = ActivationTokenModel(user=new_user)
+        db.add(activation_token)
+        await db.flush()
+        await db.refresh(activation_token)
+        token_value = activation_token.token
 
-    await db.add(new_user)
-    await db.commit()
-    await db.refresh(new_user)
 
-    return {
-        "message": "Created succesfully, your account is inactive, to activate use activation token",
-        "activation token": activation_token.token
-    }
+        await db.commit()
+        await db.refresh(new_user)
+
+    except SQLAlchemyError as e:
+        await db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="An error occurred during user creation."
+        ) from e
+    else:
+        BASE_URL = str(request.base_url)
+        activation_link = f"{BASE_URL}accounts/activate/?token={token_value}"
+        background_tasks.add_task(
+            email_sender.send_activation_email,
+            str(user_data.email),
+            activation_link
+        )
+        return {"message": "User registered successfully. Please check your email to activate your account.",}
