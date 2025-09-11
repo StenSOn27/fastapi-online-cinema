65ad73efedfd8805886d8942c20c02d8acf1d495
StenSOn27 <tarashevchik27@gmail.com>
2025-09-11 21:54:53 +0000

    congigure base settings

---
+++ src/config/settings.py
@@ -0,0 +1,17 @@
+import os
+from pathlib import Path
+from pydantic_settings import BaseSettings
+
+
+class BaseAppSettings(BaseSettings):
+    BASE_DIR: Path = Path(__file__).parent.parent
+
+    PATH_TO_EMAIL_TEMPLATES_DIR: str = str(BASE_DIR / "notifications" / "templates")
+    ACTIVATION_EMAIL_TEMPLATE_NAME: str = "activation_request.html"
+
+    EMAIL_HOST: str = os.getenv("EMAIL_HOST", "localhost")
+    EMAIL_PORT: int = int(os.getenv("EMAIL_PORT", 1025))
+    EMAIL_HOST_USER: str = os.getenv("EMAIL_HOST_USER", "testuser")
+    EMAIL_HOST_PASSWORD: str = os.getenv("EMAIL_HOST_PASSWORD", "test_password")
+    EMAIL_USE_TLS: bool = os.getenv("EMAIL_USE_TLS", "False").lower() == "true"
+    MAILHOG_API_PORT: int = os.getenv("MAILHOG_API_PORT", 8025)
