720daee984c28a252eed63cbb71dbb14d414475a
StenSOn27 <tarashevchik27@gmail.com>
2025-09-11 13:02:45 +0000

    add await to database requests

--- src/routes/accounts.py
+++ src/routes/accounts.py
@@ -14,17 +14,18 @@ async def register_user(
     user_data: UserRegisterRequestSchema,
     db: AsyncSession = Depends(get_db)
 ):
-    result = db.execute(select(UserModel).where(email=user_data.email))
+    result = await db.execute(select(UserModel).where(email=user_data.email))
     existing_user = result.scalars().first()
     if existing_user:
         raise HTTPException(status_code=400, detail="User already exists")
 
-    default_group = db.execute(
+    default_group = await db.execute(
         select(UserGroupModel)
         .filter_by(name=UserGroupEnum.USER)
     ).scalars().first()
 
     hashed_password = hash_password(user_data.password)
+
     new_user = UserModel(
         email=user_data.email,
         _hashed_password=hashed_password,
@@ -34,11 +35,11 @@ async def register_user(
 
     activation_token = ActivationTokenModel(user=new_user)
 
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
+    await db.add(new_user)
+    await db.commit()
+    await db.refresh(new_user)
 
     return {
         "message": "Created succesfully, your account is inactive, to activate use activation token",
-        "activation token": activation_token
+        "activation token": activation_token.token
     }
