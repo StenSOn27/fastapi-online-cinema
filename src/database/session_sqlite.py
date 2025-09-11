8bb583bf8a6d4be6eddc0891b3fcb93ad0de8a9c
StenSOn27 <tarashevchik27@gmail.com>
2025-09-11 22:44:45 +0000

    fix session bug

--- src/database/session_sqlite.py
+++ src/database/session_sqlite.py
@@ -6,9 +6,9 @@ from sqlalchemy.ext.asyncio import (
 DATABASE_URL = "sqlite+aiosqlite:///./database.db"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
-AsyncSession = async_sessionmaker(engine, expire_on_commit=False)
+async_session = async_sessionmaker(engine, expire_on_commit=False)
 
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
-    async with AsyncSession as db:
+    async with async_session() as db:
         yield db
