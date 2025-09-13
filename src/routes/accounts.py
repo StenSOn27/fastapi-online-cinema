85e5adf6c44901d62c01b3ecb00a32929a49ff2e
StenSOn27 <tarashevchik27@gmail.com>
2025-09-13 20:13:34 +0000

    implement activate endpoint

--- src/routes/accounts.py
+++ src/routes/accounts.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, Request, status, BackgroundTasks
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
 from src.crud import get_user_by_email
 from src.schemas.accounts import UserRegisterRequestSchema
 from src.database.models.accounts import ActivationTokenModel, UserGroupEnum, UserModel, UserGroupModel
@@ -81,3 +82,44 @@ async def register_user(
             activation_link
         )
         return {"message": "User registered successfully. Please check your email to activate your account.",}
+
+
+@router.post("/activate/")
+async def activate_user(
+    token: str, request: Request,
+    background_tasks: BackgroundTasks,
+    db: AsyncSession = Depends(get_db),
+    email_sender: EmailSenderInterface = Depends(get_accounts_email_notificator),
+):
+
+    stmt = select(ActivationTokenModel).options(selectinload(ActivationTokenModel.user))
+    result = await db.execute(stmt.filter_by(token=token))
+    activation_token = result.scalars().first()
+    print(activation_token)
+    if not activation_token:
+        raise HTTPException(status_code=400, detail="Invalid or expired activation token.")
+
+    user = activation_token.user
+    if user.is_active:
+        raise HTTPException(status_code=400, detail="User account is already active.")
+
+    try:
+        user.is_active = True
+        await db.delete(activation_token)
+        await db.commit()
+        await db.refresh(user)
+    except SQLAlchemyError as e:
+        await db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="An error occurred during account activation."
+        ) from e
+    else:
+        BASE_URL = str(request.base_url)
+        login_link = f"{BASE_URL}accounts/login/"
+        background_tasks.add_task(
+            email_sender.send_activation_complete_email,
+            str(user.email),
+            login_link
+        )
+        return {"message": "Account activated successfully."}
