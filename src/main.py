a92a4caa44fbf5bb44ba84de6a413f7ea977199d
Taras Shevchyk <tarashevchik27@gmail.com>
2025-09-24 17:37:41 +0300

    update routes configuration

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
-from src.routes.accounts import router
+from src.routes.accounts import router as account_router
+from src.routes.movies import router as movie_router
+
 
 app = FastAPI()
 
-app.include_router(router)
+api_version_prefix = "/api/v1"
+
+app.include_router(account_router, prefix=api_version_prefix)
+app.include_router(movie_router, prefix=api_version_prefix)
