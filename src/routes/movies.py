88a45eb54788a43662b168f12382952e0a194eae
Taras Shevchyk <tarashevchik27@gmail.com>
2025-09-24 18:58:32 +0300

    implement movie like/dislike endpoints

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from schemas.movies import MovieListItem, MovieRetrieve
 from src.database.models.movies import Movie, MovieLike, ReactionType, User
 from src.config.dependencies import get_db, get_current_user
+from sqlalchemy.exc import IntegrityError
 
 
 router = APIRouter(prefix="/movies")
@@ -24,3 +25,46 @@ async def get_movie(
 ) -> MovieRetrieve:
     movie = (await db.scalars(select(Movie).where(Movie.id == movie_id))).first()
     return MovieRetrieve.model_validate(movie)
+
+@router.post("/{movie_id}/like")
+async def like_movie(
+    movie_id: int,
+    db: AsyncSession = Depends(get_db),
+    user: User = Depends(get_current_user)
+) -> dict:
+
+    movie = (await db.scalars(select(Movie).where(Movie.id == movie_id))).first()
+    if not movie:
+        raise HTTPException(status_code=404, detail="Movie not found")
+
+    like = MovieLike(user_id=user.id, movie_id=movie_id, is_like=True)
+
+    try:
+        db.add(like)
+        await db.commit()
+    except IntegrityError:
+        raise HTTPException(status_code=400, detail="You already liked this movie")
+
+    return {"message": "Movie liked"}
+
+@router.post("/{movie_id}/dislike")
+async def dislike_movie(
+    movie_id: int,
+    db: AsyncSession = Depends(get_db),
+    user: User = Depends(get_current_user)
+) -> dict:
+
+    movie = (await db.scalars(select(Movie).where(Movie.id == movie_id))).first()
+    if not movie:
+        raise HTTPException(status_code=404, detail="Movie not found")
+
+    dislike = MovieLike(user_id=user.id, movie_id=movie_id, is_like=False)
+
+    try:
+        db.add(dislike)
+        await db.commit()
+    except IntegrityError:
+        raise HTTPException(status_code=400, detail="You already disliked this movie")
+
+    return {"message": "Movie disliked"}
+
