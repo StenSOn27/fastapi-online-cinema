a6e4613ba47d671c22b2f9d14bb64296c44eee57
Taras Shevchyk <tarashevchik27@gmail.com>
2025-10-02 03:21:20 +0300

    implement functions for getting genres/stars/directors by ids

--- src/crud.py
+++ src/crud.py
@@ -1,7 +1,9 @@
 import datetime
+from typing import List
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from src.database.models.accounts import ActivationTokenModel, UserModel
+from src.database.models.movies import Genre, Star, Director
 
 
 async def get_user_by_email(db: AsyncSession, email: str) -> UserModel | None:
@@ -17,3 +19,18 @@ def delete_expired_tokens(db):
         ActivationTokenModel.expires_at < now
     ).delete(synchronize_session=False)
     db.commit()
+
+
+async def get_genres_by_ids(db: AsyncSession, ids: list[int]) -> list[Genre]:
+    result = await db.execute(select(Genre).where(Genre.id.in_(ids)))
+    return result.scalars().all()
+
+
+async def get_stars_by_ids(db: AsyncSession, ids: list[int]) -> list[Star]:
+    result = await db.execute(select(Star).where(Star.id.in_(ids)))
+    return result.scalars().all()
+
+
+async def get_directors_by_ids(db: AsyncSession, ids: list[int]) -> list[Director]:
+    result = await db.execute(select(Director).where(Director.id.in_(ids)))
+    return result.scalars().all()
