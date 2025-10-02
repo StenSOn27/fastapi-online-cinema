b15b1f024c118a258ccadbac6a50d3711e0577d2
Taras Shevchyk <tarashevchik27@gmail.com>
2025-10-02 17:42:42 +0300

    implement endpoint to work with shoping cart

---
+++ src/routes/admin/admin_cart.py
@@ -0,0 +1,42 @@
+from typing import List
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from sqlalchemy.orm import selectinload
+from sqlalchemy.exc import IntegrityError
+from src.database.models.movies import Movie
+from src.database.models.shopping_cart import Cart, CartItem
+from src.database.session_sqlite import get_db
+from src.config.dependencies import require_roles
+from src.schemas.accounts import UserRetrieveSchema
+from src.schemas.cart import CartMovieItem
+
+router = APIRouter(prefix="/cart/admin")
+
+@router.get(
+    "/user/{user_id}",
+    response_model=List[CartMovieItem],
+    dependencies=[Depends(require_roles(["ADMIN"]))]
+)
+async def get_user_cart(
+    user_id: int,
+    db: AsyncSession = Depends(get_db),
+):
+    try:
+        result = await db.execute(
+            select(CartItem)
+            .join(Cart)
+            .where(Cart.user_id == user_id)
+            .options(
+                selectinload(CartItem.movie).selectinload(Movie.genres)
+            )
+        )
+        cart_items = result.scalars().all()
+    
+    except IntegrityError:
+        raise HTTPException(
+            status_code=500,
+            detail="An unexpected database error occurred while retrieving the user's cart. Please try again later."
+        )
+
+    return [CartMovieItem.model_validate(item.movie) for item in cart_items]
