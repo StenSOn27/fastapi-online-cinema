ca3b26d39421b4b3c2c4ff8678433e1db2a70581
StenSOn27 <tarashevchik27@gmail.com>
2025-10-07 16:08:09 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,11 +2,8 @@
 
 export PYTHONPATH=$PYTHONPATH:/app
 
-# Чекаємо на базу
 python src/database/wait_for_db.py
 
-# Запускаємо всі міграції
 alembic upgrade head
 
-# Запускаємо FastAPI як основний процес контейнера
 exec uvicorn src.main:app --host 0.0.0.0 --port 8000 --reload
